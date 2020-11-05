179b0e99123080ec638a0a18f5e1f3610bd8ec7d
Kaylon <kcyoung@tiksdd.com>
2020-11-05 12:04:41 -0600

    Updated app.js to add all records then remove on button click.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,7 +4,27 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.select("#button")
 
+tableData.forEach(incident => {
+    console.log(incident);
+  
+    //Append one table row 
+    var row = tbody.append("tr");
+  
+    //Log each incident report value
+    Object.entries(incident).forEach(([key, value]) => {
+      console.log(key, value);
+  
+      //Append a cell per incident report value
+      var cell = row.append("td");
+  
+      // Add each cell text value
+      cell.text(value);
+    });
+  });
+
 button.on("click", function() {
+    d3.selectAll("tr").remove();
+    
     var inputElement = d3.select("#inputDate");
 
     var inputValue = inputElement.property("value");
@@ -19,23 +39,6 @@ button.on("click", function() {
 })
 
 
-// tableData.forEach(incident => {
-//     console.log(incident);
-  
-//     //Append one table row 
-//     var row = tbody.append("tr");
-  
-//     //Log each incident report value
-//     Object.entries(incident).forEach(([key, value]) => {
-//       console.log(key, value);
-  
-//       //Append a cell per incident report value
-//       var cell = row.append("td");
-  
-//       // Add each cell text value
-//       cell.text(value);
-//     });
-//   });
   
 
 // YOUR CODE HERE!
