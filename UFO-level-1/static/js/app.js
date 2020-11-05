a5f647f24a3d493d383cf4b1ff08ab5639583676
Kaylon <kcyoung@tiksdd.com>
2020-11-05 11:53:51 -0600

    Updated form elements on index.html.  Updated app.js to include event form filter. Updated style.css

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,24 +2,40 @@
 var tableData = data;
 
 var tbody = d3.select("tbody");
+var button = d3.select("#button")
 
-tableData.forEach(incident => {
-    console.log(incident);
+button.on("click", function() {
+    var inputElement = d3.select("#inputDate");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    
+    var filteredData = tableData.filter(incident => incident.datetime === inputValue);
+
+    console.log(filteredData);
+
+})
+
+
+// tableData.forEach(incident => {
+//     console.log(incident);
   
-    //Append one table row 
-    var row = tbody.append("tr");
+//     //Append one table row 
+//     var row = tbody.append("tr");
   
-    //Log each incident report value
-    Object.entries(incident).forEach(([key, value]) => {
-      console.log(key, value);
+//     //Log each incident report value
+//     Object.entries(incident).forEach(([key, value]) => {
+//       console.log(key, value);
   
-      //Append a cell per incident report value
-      var cell = row.append("td");
+//       //Append a cell per incident report value
+//       var cell = row.append("td");
   
-      // Add each cell text value
-      cell.text(value);
-    });
-  });
+//       // Add each cell text value
+//       cell.text(value);
+//     });
+//   });
   
 
 // YOUR CODE HERE!
