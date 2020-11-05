2a66e115a2086e121f49b5549363b781a4580b8f
Kaylon <kcyoung@tiksdd.com>
2020-11-05 10:47:46 -0600

    Updated app.js to display table contents.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,25 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+
+tableData.forEach(incident => {
+    console.log(incident);
+  
+    //Append one table row 
+    var row = tbody.append("tr");
+  
+    //Log each incident report value
+    Object.entries(incident).forEach(([key, value]) => {
+      console.log(key, value);
+  
+      //Append a cell per incident report value
+      var cell = row.append("td");
+  
+      // Add each cell text value
+      cell.text(value);
+    });
+  });
+  
+
 // YOUR CODE HERE!
