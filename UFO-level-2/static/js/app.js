56bcf953eb79fdfbc0c3ed9a0756a70d575d6b87
Kaylon <kcyoung@tiksdd.com>
2020-11-09 20:05:15 -0600

    Removed unnecessary code. Added comments to code.

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -3,22 +3,24 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
+// select tags and assign them to a variable
 var tbody = d3.select("tbody");
 var buttonFilter = d3.select("#buttonFilter")
 var buttonClear = d3.select("#buttonClear")
 
+// Create a function to create table rows and cells filled with data
 function showTableInfo(tableInfo){
     tableInfo.forEach(incident => {
         console.log(incident);
       
-        //Append one table row 
+        // Append one table row 
         var row = tbody.append("tr").attr("class", "incident");
       
-        //Log each incident report value
+        // Log each incident report value
         Object.entries(incident).forEach(([key, value]) => {
           console.log(key, value);
       
-          //Append a cell per incident report value
+          // Append a cell per incident report value
           var cell = row.append("td");
       
           // Add each cell text value
@@ -27,33 +29,37 @@ function showTableInfo(tableInfo){
     });
 }
 
+// Create a function to filter data by multiple 
 function createFilter(tableInfo){
     
+    // Assign form fields to variables
     var inputDateElement = d3.select("#inputDate");
     var inputCityElement = d3.select("#inputCity");
     var inputStateElement = d3.select("#inputState");
     var inputCountryElement = d3.select("#inputCountry");
     var inputShapeElement = d3.select("#inputShape");
 
+    // Assign values of form elements to variables
     var inputDateValue = inputDateElement.property("value");
     var inputCityValue = inputCityElement.property("value");
     var inputStateValue = inputStateElement.property("value");
     var inputCountryValue = inputCountryElement.property("value");
     var inputShapeValue = inputShapeElement.property("value");
 
+    // Assign values of checked radio button to a variable.
     var radioChecked = d3.select('input[name="inlineRadioOptions"]:checked').node().value
 
-    console.log("radio: ", radioChecked)
-      
+    //Check if Or radio button is checked, filter using 'or' operator        
     if (radioChecked === "or"){
         var finalFiltered  = tableInfo.filter(incident => incident.datetime === inputDateValue || incident.city.toLowerCase() === inputCityValue.toLowerCase() ||
         incident.state.toLowerCase() === inputStateValue.toLowerCase() || incident.country.toLowerCase() === inputCountryValue.toLowerCase() ||
         incident.shape.toLowerCase() === inputShapeValue.toLowerCase())
         
         console.log(finalFiltered)
-
+        // Return values from filter
         return finalFiltered
-    }  
+    } 
+    // If And radio button is checked, filter fields using 'And' operator and return values.
     else {
         if ( inputDateValue !== ""){
             var dateFiltered = tableInfo.filter(incident => incident.datetime === inputDateValue);  
@@ -63,23 +69,17 @@ function createFilter(tableInfo){
         if (inputCityValue !== "" ){
             var cityFiltered = dateFiltered.filter(incident => incident.city.toLowerCase() === inputCityValue.toLowerCase());
         }
-        else {var cityFiltered = dateFiltered}
-    
-        // console.log(cityFiltered);
+        else {var cityFiltered = dateFiltered}   
     
         if (inputStateValue !== "" ){
             var stateFiltered = cityFiltered.filter(incident => incident.state.toLowerCase() === inputStateValue.toLowerCase());
         }
         else {var stateFiltered = cityFiltered}
     
-        // console.log(stateFiltered);
-    
         if (inputCountryValue !== "" ){
             var countryFiltered = stateFiltered.filter(incident => incident.country.toLowerCase() === inputCountryValue.toLowerCase());
         }
         else {var countryFiltered = stateFiltered}
-    
-        // console.log(countryFiltered);
     
         if (inputShapeValue !== "" ){
             var finalFiltered = countryFiltered.filter(incident => incident.shape.toLowerCase() === inputShapeValue.toLowerCase());
